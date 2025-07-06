c73c0f8ca21cc029d4605fa0fec2bd3e98e43ceb
Adebanjo <navigatorabraham17@gmail.com>
2025-07-06 17:20:33 +0100

    testing

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+'use client';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="p-8 text-center">
+      <h1 className="text-4xl font-bold text-primary">Welcome to DevSpring</h1>
+      <p className="mt-4 text-gray-600">Support and create campaigns for developers!</p>
+    </main>
+  );
+};
+
+export default Home;
